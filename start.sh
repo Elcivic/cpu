0166c5227b63f1dd36c3ecca51b57b3e8e90811f
Elcivic <80659425+Elcivic@users.noreply.github.com>
2021-03-14 23:53:51 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qdutjk93vqj8mk8jf8ex344ha2cwg8yfxc5t52y.test1
 
 
 done
